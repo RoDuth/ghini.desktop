RoDuth/ghini.desktop

commit 9be4b3ac59cdee333cb362a1474ba12866415870

Copy, paste, delete and show geojson.
Provides methods to use spatial data other than reports and shapefiles.
Show requires a kml viewer to be installed and associated to .kml file
type.

Also: clean up loc_editor.glade

bauble/editor.py
@@ -24,6 +24,7 @@
 import datetime
 import os
 import weakref
+import json
 from pathlib import Path
 from random import random
 from typing import Optional
@@ -35,6 +36,7 @@ logger = logging.getLogger(__name__)
 import dateutil.parser as date_parser
 
 from gi.repository import Gtk
+from gi.repository import Gio
 from gi.repository import GLib
 from gi.repository import GdkPixbuf
 
@@ -1765,6 +1767,107 @@ class GenericEditorPresenter:
         self.clear_problems()
         if isinstance(self.view, GenericEditorView):
             self.view.cleanup()
+
+
+class PresenterMapMixin:
+    """Mixin for presenters that include a map GtkMenuButton.
+
+    Classes that use this mixin must:
+    - subclass `GenericEditorPresenter`
+    - have a `view` attribute that points to an instance of `GenericEditorView`
+    - provide a GtkMenuButton widget named `map_menu_btn` that contains a
+      GtkImage widget named `map_btn_icon` within the view's widgets
+    - supply a valid string path to a mako kml template via the
+      `self.kml_template` attribute
+    - call `self.init_map_menu()`
+    - have a `model` attribute with a `geojson` attribute that returns valid a
+      geojson feature geometry part with "type" and "coordinates" keys.
+    """
+
+    def __init__(self):
+        self.kml_template = None
+
+    def on_map_copy(self, *_args):
+        # convert to JSON string and copy to clipboard
+        geojson = json.dumps(self.model.geojson)
+        if bauble.gui:
+            bauble.gui.get_display_clipboard().set_text(geojson, -1)
+
+    def on_map_paste(self, *_args):
+        if bauble.gui:
+            text = bauble.gui.get_display_clipboard().wait_for_text()
+            try:
+                geojson = json.loads(text)
+                # basic validation...
+                if not set(geojson.keys()) == {'type', 'coordinates'}:
+                    raise AttributeError
+                self.model.geojson = geojson
+                self._dirty = True
+                self.refresh_sensitivity()
+            except (AttributeError, json.JSONDecodeError) as e:
+                logger.debug('geojson paste %s(%s)', type(e).__name__, e)
+                logger.debug('geojson paste %s', text)
+                self.view.run_message_dialog(
+                    _('Paste failed, invalid geojson?')
+                )
+        self.init_map_menu()
+
+    def on_map_delete(self, *_args):
+        if self.view.run_yes_no_dialog(
+            _('Are you sure you want to delete geojson data?'), yes_delay=1
+        ):
+            self.model.geojson = None
+            self._dirty = True
+            self.refresh_sensitivity()
+            self.init_map_menu()
+
+    def on_map_kml_show(self, *_args):
+        import tempfile
+        from mako.template import Template
+        template = Template(filename=self.kml_template,
+                            input_encoding='utf-8',
+                            output_encoding='utf-8')
+        file_handle, filename = tempfile.mkstemp(suffix='.kml')
+        out = template.render(value=self.model)
+        os.write(file_handle, out)
+        os.close(file_handle)
+        try:
+            utils.desktop.open(filename)
+        except OSError:
+            self.view.run_message_dialog(
+                _('Could not open the kml file. You can open the file '
+                  'manually at %s') % filename
+            )
+
+    def init_map_menu(self):
+        menu = Gio.Menu()
+        action_name = self.model.__tablename__.lower() + '_map'
+        action_group = Gio.SimpleActionGroup()
+        menu_items = (
+            (_('Copy'), 'copy', self.on_map_copy),
+            (_('Paste'), 'paste', self.on_map_paste),
+            (_('Delete'), 'delete', self.on_map_delete),
+            (_('Show'), 'show', self.on_map_kml_show),
+        )
+        if not self.model.geojson:
+            menu_items = (menu_items[1],)
+            self.view.widgets.map_btn_icon.set_from_icon_name(
+                'location-services-disabled-symbolic', Gtk.IconSize.BUTTON
+            )
+        else:
+            self.view.widgets.map_btn_icon.set_from_icon_name(
+                'location-services-active-symbolic', Gtk.IconSize.BUTTON
+            )
+        for label, name, handler in menu_items:
+            action = Gio.SimpleAction.new(name, None)
+            action.connect("activate", handler)
+            action_group.add_action(action)
+            menu_item = Gio.MenuItem.new(label, f'{action_name}.{name}')
+            menu.append_item(menu_item)
+
+        map_menu_btn = self.view.widgets.map_menu_btn
+        map_menu_btn.set_menu_model(menu)
+        map_menu_btn.insert_action_group(action_name, action_group)
 
 
 class ChildPresenter(GenericEditorPresenter):

bauble/plugins/garden/location.py
@@ -21,11 +21,12 @@ Location table definition and related
 """
 import os
 import traceback
+from pathlib import Path
 
 import logging
 logger = logging.getLogger(__name__)
 
-from gi.repository import Gtk  # noqa
+from gi.repository import Gtk
 
 from sqlalchemy import Column, Unicode, UnicodeText
 from sqlalchemy.orm import relationship, backref, validates, deferred
@@ -38,7 +39,8 @@ from bauble import btypes as types
 from bauble.editor import (GenericModelViewPresenterEditor,
                            GenericEditorView,
                            GenericEditorPresenter,
-                           StringOrNoneValidator)
+                           StringOrNoneValidator,
+                           PresenterMapMixin)
 from bauble import utils
 from bauble import paths
 from bauble import editor
@@ -258,7 +260,7 @@ class LocationEditorView(GenericEditorView):
         self.widgets.loc_next_button.set_sensitive(sensitive)
 
 
-class LocationEditorPresenter(GenericEditorPresenter):
+class LocationEditorPresenter(GenericEditorPresenter, PresenterMapMixin):
 
     widget_to_field_map = {'loc_name_entry': 'name',
                            'loc_code_entry': 'code',
@@ -305,6 +307,8 @@ class LocationEditorPresenter(GenericEditorPresenter):
                                  on_location_select)
         self.view.connect('loc_merge_button', 'clicked',
                           self.on_loc_merge_button_clicked)
+        self.kml_template = str(Path(__file__).resolve().parent / 'loc.kml')
+        self.init_map_menu()
 
     def on_loc_merge_button_clicked(self, _entry, *_args):
         entry_widget = self.view.widgets.loc_merge_entry

bauble/plugins/garden/plant.py
@@ -1,7 +1,7 @@
 # Copyright 2008-2010 Brett Adams
 # Copyright 2015-2017 Mario Frasca <mario@anche.no>.
 # Copyright 2017 Jardín Botánico de Quito
-# Copyright 2020-2021 Ross Demuth <rossdemuth123@gmail.com>
+# Copyright 2020-2022 Ross Demuth <rossdemuth123@gmail.com>
 #
 # This file is part of ghini.desktop.
 #
@@ -26,11 +26,12 @@ Defines the plant table and handled editing plants
 import os
 import traceback
 from random import random
+from pathlib import Path
 
 import logging
 logger = logging.getLogger(__name__)
 
-from gi.repository import Gtk  # noqa
+from gi.repository import Gtk
 
 from sqlalchemy import or_, and_, func, event, tuple_, not_
 from sqlalchemy import (ForeignKey, Column, Unicode, Integer, UnicodeText,
@@ -47,19 +48,22 @@ from pyparsing import (Word, removeQuotes, delimitedList, OneOrMore, oneOf,
 
 from bauble import db
 from bauble.error import CheckConditionError
-from bauble.editor import (GenericEditorView, GenericEditorPresenter,
-                           GenericModelViewPresenterEditor, NotesPresenter,
-                           PicturesPresenter)
+from bauble.editor import (GenericEditorView,
+                           GenericEditorPresenter,
+                           GenericModelViewPresenterEditor,
+                           NotesPresenter,
+                           PicturesPresenter,
+                           PresenterMapMixin)
 from bauble import meta
 from bauble import paths
-from bauble.plugins.garden.location import Location, LocationEditor
-from bauble.plugins.garden.propagation import PlantPropagation
 from bauble import prefs
 from bauble.search import SearchStrategy
 from bauble import btypes as types
 from bauble import utils
 from bauble.view import (InfoBox, InfoExpander, PropertiesExpander,
                          LinksExpander, select_in_search_results, Action)
+from .location import Location, LocationEditor
+from .propagation import PlantPropagation
 from .accession import Accession
 
 # TODO: might be worthwhile to have a label or textview next to the
@@ -1002,7 +1006,7 @@ def acc_match_func(completion: Gtk.EntryCompletion,
     return acc_to_string_matcher(accession, key)
 
 
-class PlantEditorPresenter(GenericEditorPresenter):
+class PlantEditorPresenter(GenericEditorPresenter, PresenterMapMixin):
 
     widget_to_field_map = {'plant_code_entry': 'code',
                            'plant_acc_entry': 'accession',
@@ -1150,6 +1154,8 @@ class PlantEditorPresenter(GenericEditorPresenter):
         self.history_expanded = False
 
         self.init_changes_history_view()
+        self.kml_template = str(Path(__file__).resolve().parent / 'plant.kml')
+        self.init_map_menu()
 
     def acc_get_completions(self, text):
         text = text.lower()

bauble/plugins/garden/loc.kml
@@ -1,0 +1,89 @@
+## vim: ft=mako:fdm=marker
+<%!
+from polylabel import polylabel
+from bauble.utils import xml_safe
+from bauble.utils.geo import transform
+from bauble.meta import get_default
+sys_crs = get_default('system_proj_string')
+%>\
+<%
+def json_to_kml_point(coords):
+    return ','.join(str(i) for i in coords + [0])
+
+def json_to_kml_line(coords):
+    return ' '.join(json_to_kml_point(i) for i in coords)
+
+def json_to_kml_poly(coords):
+    return ' '.join(json_to_kml_point(i) for i in coords[0])
+
+point = line = poly = None
+
+if value.geojson and sys_crs:
+  # incase another system crs is used.
+  geojson = transform(value.geojson, in_crs=sys_crs.value, out_crs='epsg:4326')
+  # should always be a polygon but allow others incase
+  if geojson.get('type') == 'Point':
+    point = json_to_kml_point(value.geojson.get('coordinates'))
+  elif geojson.get('type') == 'LineString':
+    line = json_to_kml_line(geojson.get('coordinates'))
+  elif geojson.get('type') == 'Polygon':
+    plygn = geojson.get('coordinates')
+    poly = json_to_kml_poly(plygn)
+    label = polylabel(plygn, precision=0.01)
+%>\
+<?xml version="1.0" encoding="UTF-8"?>
+<kml xmlns="http://www.opengis.net/kml/2.2" xmlns:gx="http://www.google.com/kml/ext/2.2">
+  <Document>
+    <name>${xml_safe(value.code)}</name>
+    <visibility>1</visibility>
+    <Placemark>
+      <name>${xml_safe(value.code)}</name>
+      <description><![CDATA[
+        <table>
+          <tr>
+            <td>CODE:</td>
+            <td>${xml_safe(value.code)}</td>
+          </tr>
+          % if value.name:
+          <tr>
+            <td>NAME:</td>
+            <td>${xml_safe(value.name)}</td>
+          </tr>
+          % endif
+          % if value.description:
+          <tr>
+            <td>DESCRIPTION:</td>
+            <td>${xml_safe(value.description)}</td>
+          </tr>
+          % endif
+        </table>
+          ]]>
+      </description>
+      <visibility>1</visibility>
+      % if point:
+      <Point>
+        <coordinates>
+          ${point}
+        </coordinates>
+      </Point>
+      % elif line:
+      <LineString>
+        <coordinates>
+          ${line}
+        </coordinates>
+      </LineString>
+      % elif poly:
+      <Polygon>
+        <altitudeMode>relativeToGround</altitudeMode>
+        <outerBoundaryIs>
+          <LinearRing>
+            <coordinates>
+              ${poly.replace(',0 ', ',0.2 ')}
+            </coordinates>
+          </LinearRing>
+        </outerBoundaryIs>
+      </Polygon>
+      % endif
+    </Placemark>
+  </Document>
+</kml>

bauble/plugins/garden/plant.kml
@@ -1,0 +1,115 @@
+## vim: ft=mako:fdm=marker
+<%!
+from polylabel import polylabel
+from bauble.utils import xml_safe
+from bauble.utils.geo import transform
+from bauble.meta import get_default
+sys_crs = get_default('system_proj_string')
+%>\
+<%
+def json_to_kml_point(coords):
+    return ','.join(str(i) for i in coords + [0])
+
+def json_to_kml_line(coords):
+    return ' '.join(json_to_kml_point(i) for i in coords)
+
+def json_to_kml_poly(coords):
+    return ' '.join(json_to_kml_point(i) for i in coords[0])
+
+point = line = poly = None
+if value.geojson and sys_crs:
+  # incase another system crs is used.
+  geojson = transform(value.geojson, in_crs=sys_crs.value, out_crs='epsg:4326')
+  if geojson.get('type') == 'Point':
+    point = json_to_kml_point(value.geojson.get('coordinates'))
+  elif geojson.get('type') == 'LineString':
+    line = json_to_kml_line(geojson.get('coordinates'))
+  elif geojson.get('type') == 'Polygon':
+    plygn = geojson.get('coordinates')
+    poly = json_to_kml_poly(plygn)
+    label = polylabel(plygn, precision=0.01)
+%>\
+<?xml version="1.0" encoding="UTF-8"?>
+<kml xmlns="http://www.opengis.net/kml/2.2" xmlns:gx="http://www.google.com/kml/ext/2.2">
+  <Document>
+    <name>${xml_safe(value)}</name>
+    <visibility>1</visibility>
+    <Placemark>
+      <name>${xml_safe(value)}</name>
+      <description><![CDATA[
+        <table>
+          <tr>
+            <td>SPECIES:</td>
+            <td>${value.accession.species_str(markup=True)}</td>
+          </tr>
+          <tr>
+            <td>QUANTITY:</td>
+            <td>${value.quantity}</td>
+          </tr>
+          % if value.accession.species.default_vernacular_name:
+          <tr>
+            <td>COMMON:</td>
+            <td>${value.accession.species.default_vernacular_name}</td>
+          </tr>
+          % endif
+          % if value.accession.species.label_distribution:
+          <tr>
+            <td>DISTRIBUTION:</td>
+            <td>${xml_safe(value.accession.species.label_distribution)}</td>
+          </tr>
+          % endif
+          <tr>
+            <td>FAMILY:</td>
+            <td>${value.accession.species.genus.family}</td>
+          </tr>
+          <tr>
+            <td>BED:</td>
+            <td>${xml_safe(value.location.code)} ${xml_safe(value.location.name or '')}</td>
+          </tr>
+        </table>
+          ]]>
+      </description>
+      <visibility>1</visibility>
+      % if point:
+      <Point>
+        <coordinates>
+          ${point}
+        </coordinates>
+      </Point>
+      % elif line:
+      <MultiGeometry>
+      % for point in line.split():
+        <Point>
+          <coordinates>
+            ${point}
+          </coordinates>
+        </Point>
+      % endfor
+        <LineString>
+          <coordinates>
+            ${line}
+          </coordinates>
+        </LineString>
+      </MultiGeometry>
+      % elif poly:
+      <MultiGeometry>
+        <Point>
+          <coordinates>
+            ${label[0]},${label[1]},0
+          </coordinates>
+        </Point>
+        <Polygon>
+          <altitudeMode>relativeToGround</altitudeMode>
+          <outerBoundaryIs>
+            <LinearRing>
+              <coordinates>
+                ${poly.replace(',0 ', ',0.2 ')}
+              </coordinates>
+            </LinearRing>
+          </outerBoundaryIs>
+        </Polygon>
+      </MultiGeometry>
+      % endif
+    </Placemark>
+  </Document>
+</kml>

bauble/plugins/garden/plant_editor.glade
@@ -391,6 +391,26 @@
                                 <property name="position">2</property>
                               </packing>
                             </child>
+                            <child>
+                              <object class="GtkMenuButton" id="map_menu_btn">
+                                <property name="visible">True</property>
+                                <property name="can-focus">True</property>
+                                <property name="focus-on-click">False</property>
+                                <property name="receives-default">True</property>
+                                <child>
+                                  <object class="GtkImage" id="map_btn_icon">
+                                    <property name="visible">True</property>
+                                    <property name="can-focus">False</property>
+                                    <property name="icon-name">location-services-active-symbolic</property>
+                                  </object>
+                                </child>
+                              </object>
+                              <packing>
+                                <property name="expand">False</property>
+                                <property name="fill">True</property>
+                                <property name="position">3</property>
+                              </packing>
+                            </child>
                           </object>
                           <packing>
                             <property name="left-attach">1</property>

bauble/utils/geo.py
@@ -98,6 +98,7 @@ def transform(geometry, in_crs=DEFAULT_IN_PROJ, out_crs=None, always_xy=False):
         return None
     coords = []
     transformer = Transformer.from_crs(in_crs, out_crs, always_xy=always_xy)
+    logger.debug('transform %s >> %s', in_crs, out_crs)
     try:
         if geometry_type == 'Polygon':
             for x, y in geometry.get('coordinates')[0]:

commit 77b147e4b2410f7c45f4f4f735f535c14ebe374f

Remove map action group on close, avoid leaks
KML map button menu as introduced in 9be4b3ac

bauble/editor.py
@@ -1783,7 +1783,8 @@ class PresenterMapMixin:
       GtkImage widget named `map_btn_icon` within the view's widgets
     - supply a valid string path to a mako kml template via the
       `self.kml_template` attribute
-    - call `self.init_map_menu()`
+    - call `self.init_map_menu()` on initializing.
+    - call `self.remove_map_action_group()` on close. (e.g. in `self.cleanup`)
     - have a `model` attribute with a `geojson` attribute that returns valid a
       geojson feature geometry part with "type" and "coordinates" keys.
     """
@@ -1844,6 +1845,12 @@ class PresenterMapMixin:
             )
 
     def init_map_menu(self):
+        """Initialise the map menu button.
+
+        Sets an appropriate icon on the button, appends an appropriate menu and
+        inserts an action group to the map_menu_btn widget.  The action group
+        should be removed on close for the editor to be garbage collected.
+        """
         menu = Gio.Menu()
         action_name = self.model.__tablename__.lower() + '_map'
         action_group = Gio.SimpleActionGroup()
@@ -1872,6 +1879,11 @@ class PresenterMapMixin:
         map_menu_btn = self.view.widgets.map_menu_btn
         map_menu_btn.set_menu_model(menu)
         map_menu_btn.insert_action_group(action_name, action_group)
+
+    def remove_map_action_group(self):
+        """Remove the action group from map_menu_btn widget."""
+        action_name = self.model.__tablename__.lower() + '_map'
+        self.view.widgets.map_menu_btn.insert_action_group(action_name, None)
 
 
 class ChildPresenter(GenericEditorPresenter):

bauble/plugins/garden/location.py
@@ -335,6 +335,10 @@ class LocationEditorPresenter(GenericEditorPresenter, PresenterMapMixin):
         )
         self.init_map_menu()
 
+    def cleanup(self):
+        super().cleanup()
+        self.remove_map_action_group()
+
     def on_loc_merge_button_clicked(self, _entry, *_args):
         entry_widget = self.view.widgets.loc_merge_entry
         if self.has_problems(entry_widget):

bauble/plugins/garden/plant.py
@@ -1517,6 +1517,7 @@ class PlantEditorPresenter(GenericEditorPresenter, PresenterMapMixin):
         # the entry is made not editable for branch mode
         self.view.widgets.plant_acc_entry.props.editable = True
         self.view.get_window().props.title = _('Plant Editor')
+        self.remove_map_action_group()
 
     def start(self):
         return self.view.start()