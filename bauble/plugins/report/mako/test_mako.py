9b841113dc58b6da05aaa396b871c6db1c887698
RoDuth <rossdemuth123@gmail.com>
2023-08-23 21:14:54 +1000

    Update copyrights

--- bauble/plugins/report/mako/test_mako.py
+++ bauble/plugins/report/mako/test_mako.py
@@ -1,7 +1,7 @@
 # Copyright (c) 2005,2006,2007,2008,2009 Brett Adams <brett@belizebotanic.org>
 # Copyright (c) 2012-2015 Mario Frasca <mario@anche.no>
 # Copyright 2017 Jardín Botánico de Quito
-# Copyright (c) 2022 Ross Demuth <rossdemuth123@gmail.com>
+# Copyright (c) 2022-2023 Ross Demuth <rossdemuth123@gmail.com>
 #
 # This file is part of ghini.desktop.
 #
