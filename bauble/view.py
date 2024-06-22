209d599a1278a5b78d623c97ce2eaf7bea2e1249
RoDuth <rossdemuth123@gmail.com>
2024-06-22 13:04:17 +1000

    Fix SearchView first row hidden on first search
    The first time a search is invoked the tree view widget was scrolled
    down one row hiding the first result, which is also selected.

--- bauble/view.py
+++ bauble/view.py
@@ -902,6 +902,8 @@ class SearchView(pluginmgr.View, Gtk.Box):
     Items are a tuple - (widget name, signal name, handler)
     """
 
+    first_run: bool = True
+
     def __init__(self):
         logger.debug("SearchView::__init__")
         super().__init__()
@@ -1443,6 +1445,13 @@ class SearchView(pluginmgr.View, Gtk.Box):
                 statusbar.push(
                     sbcontext_id,
                     _("size of non homogeneous result: %s") % len(results),
+                )
+            if self.first_run:
+                # not sure why the first row is hidden on the first search but
+                # this fixes it
+                self.first_run = False
+                GLib.idle_add(
+                    self.results_view.set_cursor, Gtk.TreePath.new_first()
                 )
             self.results_view.set_cursor(Gtk.TreePath.new_first())
 
