RoDuth/ghini.desktop

commit aa26844bbb4ec6c17780984592b9e7273600ffbf

Fix _hide_restore_pic_pane and garden_map populate
size-allocate signal is triggered multiple times, often for little
reason so no need to connect to other signals which are triggered at
the same time.
Avoid multiple calls to populate the garden map by using a timeout.

Also, use a similar approach for setting the pic_pane position AFTER its
size is allocated (no need for idle_add).

bauble/view.py
@@ -721,6 +721,8 @@ class PicturesScroller(Gtk.ScrolledWindow):
         self.count = 0
         self.waiting_on_realise = 0
         self.selection: list[db.Base] = []
+        pic_pane.connect("size-allocate", self.on_pic_pane_size_allocation)
+        self._set_pic_pane_pos_timer_id = None
 
     def on_scrolled(self, adjustment: Gtk.Adjustment) -> None:
         """On scrolling add more pictures as needed.
@@ -770,6 +772,10 @@ class PicturesScroller(Gtk.ScrolledWindow):
     def _hide_restore_pic_pane(self, selection: list[db.Base] | None) -> None:
         if self.last_result_succeed:
             self.restore_position = self.pic_pane.get_position()
+            logger.debug(
+                "_hide_restore_pic_pane succeeded with %s",
+                self.restore_position,
+            )
         if self.first_run:
             if bauble.gui:
                 width = bauble.gui.window.get_size().width
@@ -781,23 +787,28 @@ class PicturesScroller(Gtk.ScrolledWindow):
                 self.pic_pane.set_position(width - 6)
             self.last_result_succeed = False
         else:
-            # successful
-            if self.restore_position:
-                if self.last_result_succeed:
-                    self.pic_pane.set_position(self.restore_position)
-                    self.first_run = False
-                else:
-                    # first_run need to wait for everything to initialise or
-                    # will get a pic_pane even when one isn't intended
-                    GLib.idle_add(
-                        self.pic_pane.set_position, self.restore_position
-                    )
-
             self.last_result_succeed = True
+
+    def _set_pic_pane_position_after_timer(self) -> None:
+        logger.debug("restoring pic_pane: %s", self.restore_position)
+        self.pic_pane.set_position(self.restore_position)
+        self._set_pic_pane_pos_timer_id = None
+
+    def on_pic_pane_size_allocation(
+        self, _pic_pane: Gtk.Paned, _allocation: Gdk.Rectangle
+    ) -> None:
+        if self._set_pic_pane_pos_timer_id:
+            return
+        if self.restore_position and self.last_result_succeed:
+            self._set_pic_pane_pos_timer_id = GLib.timeout_add(
+                300, self._set_pic_pane_position_after_timer
+            )
+            self.first_run = False
 
     def populate_from_selection(self, selection: list[db.Base] | None) -> None:
         logger.debug("PicturesScroller.populate_from_selection(%s)", selection)
 
+        self._hide_restore_pic_pane(selection)
         # bail early if nothing has changed
         if self.selection == selection:
             return
@@ -808,7 +819,6 @@ class PicturesScroller(Gtk.ScrolledWindow):
 
         for kid in self.pictures_box.get_children():
             kid.destroy()
-        self._hide_restore_pic_pane(selection)
 
         self.count = 0
         self.waiting_on_realise = 0

bauble/test/test_view.py
@@ -17,6 +17,7 @@
 # along with ghini.desktop. If not, see <http://www.gnu.org/licenses/>.
 
 import os
+import time
 from datetime import datetime
 from pathlib import Path
 from unittest import mock
@@ -1856,12 +1857,14 @@ class TestPicturesScroller(BaubleTestCase):
         self.assertIsNone(picture_scroller.restore_position)
         # no result
         picture_scroller._hide_restore_pic_pane([])
+        picture_scroller.pic_pane.emit("size-allocate", Gdk.Rectangle())
         update_gui()
         self.assertIsNone(picture_scroller.restore_position)
         self.assertFalse(picture_scroller.last_result_succeed)
         self.assertTrue(picture_scroller.first_run)
         # error
         picture_scroller._hide_restore_pic_pane(None)
+        picture_scroller.pic_pane.emit("size-allocate", Gdk.Rectangle())
         update_gui()
         self.assertIsNone(picture_scroller.restore_position)
         self.assertFalse(picture_scroller.last_result_succeed)
@@ -1869,11 +1872,13 @@ class TestPicturesScroller(BaubleTestCase):
         # success (but last result was a fail - idle_add pic_pane set_position)
         restore_pos = picture_scroller.restore_position
         picture_scroller._hide_restore_pic_pane([1])
+        picture_scroller.pic_pane.emit("size-allocate", Gdk.Rectangle())
         update_gui()
         self.assertTrue(picture_scroller.last_result_succeed)
         self.assertTrue(picture_scroller.first_run)
         # no result (yet records restore position because prev was successful)
         picture_scroller._hide_restore_pic_pane([])
+        picture_scroller.pic_pane.emit("size-allocate", Gdk.Rectangle())
         update_gui()
         self.assertFalse(picture_scroller.last_result_succeed)
         restore_pos2 = picture_scroller.restore_position
@@ -1882,10 +1887,12 @@ class TestPicturesScroller(BaubleTestCase):
         self.assertTrue(picture_scroller.first_run)
         # succeed and change position then succeed again should store new value
         picture_scroller._hide_restore_pic_pane([1])
+        picture_scroller.pic_pane.emit("size-allocate", Gdk.Rectangle())
         update_gui()
         picture_scroller.pic_pane.set_position(100)
         self.assertTrue(picture_scroller.last_result_succeed)
         picture_scroller._hide_restore_pic_pane([1])
+        picture_scroller.pic_pane.emit("size-allocate", Gdk.Rectangle())
         update_gui()
         self.assertEqual(picture_scroller.restore_position, 100)
         self.assertTrue(picture_scroller.last_result_succeed)
@@ -1894,20 +1901,27 @@ class TestPicturesScroller(BaubleTestCase):
         # successful position
         picture_scroller.pic_pane.set_position(50)
         picture_scroller._hide_restore_pic_pane(None)
+        picture_scroller.pic_pane.emit("size-allocate", Gdk.Rectangle())
         update_gui()
         self.assertFalse(picture_scroller.last_result_succeed)
         picture_scroller._hide_restore_pic_pane(None)
+        picture_scroller.pic_pane.emit("size-allocate", Gdk.Rectangle())
         update_gui()
         self.assertFalse(picture_scroller.last_result_succeed)
         picture_scroller._hide_restore_pic_pane([])
+        picture_scroller.pic_pane.emit("size-allocate", Gdk.Rectangle())
         update_gui()
         self.assertFalse(picture_scroller.last_result_succeed)
         self.assertEqual(picture_scroller.restore_position, 50)
         picture_scroller._hide_restore_pic_pane([1])
+        picture_scroller.pic_pane.emit("size-allocate", Gdk.Rectangle())
+        update_gui()
+        # Wait for timeout
+        time.sleep(0.5)
         update_gui()
         self.assertTrue(picture_scroller.last_result_succeed)
-        self.assertEqual(picture_scroller.pic_pane.get_position(), 50)
         self.assertEqual(picture_scroller.restore_position, 50)
+        self.assertEqual(picture_scroller.pic_pane.get_position(), 50)
 
 
 class GlobalFunctionsTests(BaubleTestCase):

bauble/plugins/garden/garden_map.py
@@ -822,6 +822,7 @@ class SearchViewMapPresenter:
         self.context_menu: Gtk.Menu
         self.init_context_menu()
         self.zoom_to_home()
+        self._resize_timer_id = None
 
     @staticmethod
     def is_visible() -> bool:
@@ -839,7 +840,7 @@ class SearchViewMapPresenter:
                 - child1.get_allocation().width
                 - 5
             )
-            logger.debug("map_is_visble width = %s", width)
+            logger.debug("map is_visble width = %s", width)
             if width > 100:
                 return view.pic_pane_notebook.get_current_page() == 0
         return False
@@ -1065,6 +1066,21 @@ class SearchViewMapPresenter:
             self.populate_thread.start()
             self.populated = True
 
+    def populate_on_size_allocation(
+        self, _pic_pane: Gtk.Paned, _allocation: Gdk.Rectangle
+    ) -> None:
+        if self._resize_timer_id:
+            return
+        self._resize_timer_id = GLib.timeout_add(
+            300, self._populate_after_timer
+        )
+
+    def _populate_after_timer(self):
+        if not self.populated and self.is_visible():
+            logger.debug("populate_after_timer")
+            self.populate_map_from_search_view()
+        self._resize_timer_id = None
+
     def populate_map_from_search_view(
         self, *_args, view: SearchView | None = None
     ) -> None:
@@ -1082,8 +1098,7 @@ class SearchViewMapPresenter:
                 model = view.results_view.get_model()
                 objs = []
                 if model:
-                    # see: https://github.com/python/mypy/issues/2220
-                    objs = [i[0] for i in model]  # type: ignore
+                    objs = [i[0] for i in model]
                 self.populate_map(objs)
             selected = view.get_selected_values()
             if selected:
@@ -1379,22 +1394,10 @@ def setup_garden_map() -> None:
 
     SearchView.pic_pane_notebook_pages.add(pic_pane_page)
 
-    switch_page_signal = (
-        "pic_pane_notebook",
-        "switch-page",
-        map_presenter.populate_map_from_search_view,
-    )
-    SearchView.extra_signals.add(switch_page_signal)
-    position_signal = (
-        "pic_pane",
-        "notify::position",
-        map_presenter.populate_map_from_search_view,
-    )
-    SearchView.extra_signals.add(position_signal)
     size_allocate_signal = (
         "pic_pane",
         "size-allocate",
-        map_presenter.populate_map_from_search_view,
+        map_presenter.populate_on_size_allocation,
     )
     SearchView.extra_signals.add(size_allocate_signal)
 
@@ -1405,8 +1408,7 @@ def setup_garden_map() -> None:
     search_view = get_search_view()
     if search_view:
         search_view.add_page_to_pic_pane_notebook(*pic_pane_page)
-        search_view.connect_signal(*switch_page_signal)
-        search_view.connect_signal(*position_signal)
+        search_view.connect_signal(*size_allocate_signal)
 
     # Listen for changes to the database and react if need be
     event.listen(
@@ -1457,23 +1459,9 @@ def expunge_garden_map() -> None:
 
     SearchView.extra_signals.remove(
         (
-            "pic_pane_notebook",
-            "switch-page",
-            map_presenter.populate_map_from_search_view,
-        )
-    )
-    SearchView.extra_signals.remove(
-        (
-            "pic_pane",
-            "notify::position",
-            map_presenter.populate_map_from_search_view,
-        )
-    )
-    SearchView.extra_signals.remove(
-        (
             "pic_pane",
             "size-allocate",
-            map_presenter.populate_map_from_search_view,
+            map_presenter.populate_on_size_allocation,
         )
     )
     SearchView.populate_callbacks.remove(map_presenter.populate_map)

bauble/plugins/garden/test_garden_map.py
@@ -21,6 +21,7 @@ Test plant map
 
 import os
 import threading
+import time
 from dataclasses import astuple
 from time import sleep
 from unittest import mock
@@ -1014,6 +1015,33 @@ class TestSearchViewMapPresenter(BaubleTestCase):
         map_.destroy()
         presenter.update_thread.join()
 
+    def test_on_size_allocation_triggers_once_if_visible(self):
+        map_ = GardenMap(Map())
+        presenter = SearchViewMapPresenter(map_)
+        presenter.populate_map_from_search_view = mock.Mock()
+        presenter.is_visible = lambda: False
+        # multiple signals emitted
+        presenter.populate_on_size_allocation(None, None)
+        presenter.populate_on_size_allocation(None, None)
+        presenter.populate_on_size_allocation(None, None)
+        presenter.populate_on_size_allocation(None, None)
+        update_gui()
+        time.sleep(0.5)
+        update_gui()
+        # is_visible = False
+        presenter.populate_map_from_search_view.assert_not_called()
+        presenter.is_visible = lambda: True
+        presenter.populate_on_size_allocation(None, None)
+        presenter.populate_on_size_allocation(None, None)
+        presenter.populate_on_size_allocation(None, None)
+        presenter.populate_on_size_allocation(None, None)
+        update_gui()
+        time.sleep(0.5)
+        update_gui()
+        # is_visible = True
+        presenter.populate_map_from_search_view.assert_called_once()
+        map_.destroy()
+
     def test_get_location_polys(self):
         for func in get_setUp_data_funcs():
             func()
@@ -1723,7 +1751,7 @@ class GlobalFunctionsTest(BaubleTestCase):
         self.assertEqual(
             start_pages - 1, len(SearchView.pic_pane_notebook_pages)
         )
-        self.assertEqual(start_signals - 3, len(SearchView.extra_signals))
+        self.assertEqual(start_signals - 1, len(SearchView.extra_signals))
         self.assertEqual(
             start_pop_callbacks - 1, len(SearchView.populate_callbacks)
         )
@@ -1751,7 +1779,7 @@ class GlobalFunctionsTest(BaubleTestCase):
         self.assertEqual(
             start_pages + 1, len(SearchView.pic_pane_notebook_pages)
         )
-        self.assertEqual(start_signals + 3, len(SearchView.extra_signals))
+        self.assertEqual(start_signals + 1, len(SearchView.extra_signals))
         self.assertEqual(
             start_pop_callbacks + 1, len(SearchView.populate_callbacks)
         )
@@ -1772,14 +1800,9 @@ class GlobalFunctionsTest(BaubleTestCase):
         )
         connect_calls = [
             mock.call(
-                "pic_pane_notebook",
-                "switch-page",
-                presenter.populate_map_from_search_view,
-            ),
-            mock.call(
                 "pic_pane",
-                "notify::position",
-                presenter.populate_map_from_search_view,
+                "size-allocate",
+                presenter.populate_on_size_allocation,
             ),
         ]
         mock_get_sv().connect_signal.assert_has_calls(connect_calls)

commit 0c83734da05e1e0c264fccc7d2700a735d1cf626

_hide_restore_pic_pane restore once
Don't use timer on size-allocate, instead bail unless its the first run
after _hide_restore_pic_pane and the last result was successful.

aa26844b introduced a glitch making it almost impossible to change the
pane size as it was size-allocate is firing while the user was trying
to change the slider position.

bauble/view.py
@@ -691,8 +691,6 @@ class PicturesScroller(Gtk.ScrolledWindow):
     pictures to display.
     """
 
-    first_run: bool = True
-
     PAGE_SIZE = 6
 
     def __init__(self, parent: Gtk.Paned, pic_pane: Gtk.Paned) -> None:
@@ -703,9 +701,11 @@ class PicturesScroller(Gtk.ScrolledWindow):
         self.pic_pane = pic_pane
         self.set_width_and_notebook_page()
         self.restore_position: int | None = prefs.prefs.get(
-            PIC_PANE_WIDTH_PREF
+            PIC_PANE_WIDTH_PREF, -1
         )
+        self.restore_pic_pane = False
         pic_pane.show_all()
+        self.pic_pane.set_position(self.restore_position)
         self.pictures_box = Gtk.FlowBox()
         self.pictures_box.set_selection_mode(Gtk.SelectionMode.NONE)
         self.add(self.pictures_box)
@@ -721,7 +721,10 @@ class PicturesScroller(Gtk.ScrolledWindow):
         self.count = 0
         self.waiting_on_realise = 0
         self.selection: list[db.Base] = []
-        pic_pane.connect("size-allocate", self.on_pic_pane_size_allocation)
+        # fires considerably less than child1 or pic_pane itself.
+        pic_pane.get_child2().connect(
+            "size-allocate", self.on_pic_pane_size_allocation
+        )
         self._set_pic_pane_pos_timer_id = None
 
     def on_scrolled(self, adjustment: Gtk.Adjustment) -> None:
@@ -770,40 +773,33 @@ class PicturesScroller(Gtk.ScrolledWindow):
             prefs.prefs[PIC_PANE_PAGE_PREF] = selected
 
     def _hide_restore_pic_pane(self, selection: list[db.Base] | None) -> None:
+        # restore once
+        self.restore_pic_pane = True
         if self.last_result_succeed:
             self.restore_position = self.pic_pane.get_position()
             logger.debug(
                 "_hide_restore_pic_pane succeeded with %s",
                 self.restore_position,
             )
-        if self.first_run:
-            if bauble.gui:
-                width = bauble.gui.window.get_size().width
-                self.pic_pane.set_position(width - 6)
         if not selection:
             # No result or error
-            if bauble.gui and not self.first_run:
+            if bauble.gui:
                 width = bauble.gui.window.get_size().width
                 self.pic_pane.set_position(width - 6)
             self.last_result_succeed = False
         else:
             self.last_result_succeed = True
 
-    def _set_pic_pane_position_after_timer(self) -> None:
-        logger.debug("restoring pic_pane: %s", self.restore_position)
-        self.pic_pane.set_position(self.restore_position)
-        self._set_pic_pane_pos_timer_id = None
-
     def on_pic_pane_size_allocation(
         self, _pic_pane: Gtk.Paned, _allocation: Gdk.Rectangle
     ) -> None:
-        if self._set_pic_pane_pos_timer_id:
-            return
-        if self.restore_position and self.last_result_succeed:
-            self._set_pic_pane_pos_timer_id = GLib.timeout_add(
-                300, self._set_pic_pane_position_after_timer
-            )
-            self.first_run = False
+        if (
+            self.restore_pic_pane
+            and self.last_result_succeed
+            and self.restore_position
+        ):
+            self.pic_pane.set_position(self.restore_position)
+        self.restore_pic_pane = False
 
     def populate_from_selection(self, selection: list[db.Base] | None) -> None:
         logger.debug("PicturesScroller.populate_from_selection(%s)", selection)

bauble/test/test_view.py
@@ -1854,71 +1854,59 @@ class TestPicturesScroller(BaubleTestCase):
         pic_pane.pack2(notebook)
         box.pack_start(pic_pane, True, True, 1)
         picture_scroller = PicturesScroller(parent=pics_box, pic_pane=pic_pane)
-        self.assertIsNone(picture_scroller.restore_position)
+        self.assertEqual(picture_scroller.restore_position, -1)
         # no result
+        self.assertFalse(picture_scroller.restore_pic_pane)
         picture_scroller._hide_restore_pic_pane([])
-        picture_scroller.pic_pane.emit("size-allocate", Gdk.Rectangle())
-        update_gui()
-        self.assertIsNone(picture_scroller.restore_position)
+        self.assertTrue(picture_scroller.restore_pic_pane)
+        notebook.emit("size-allocate", Gdk.Rectangle())
+        self.assertFalse(picture_scroller.restore_pic_pane)
+        self.assertEqual(picture_scroller.restore_position, -1)
         self.assertFalse(picture_scroller.last_result_succeed)
-        self.assertTrue(picture_scroller.first_run)
         # error
         picture_scroller._hide_restore_pic_pane(None)
-        picture_scroller.pic_pane.emit("size-allocate", Gdk.Rectangle())
-        update_gui()
-        self.assertIsNone(picture_scroller.restore_position)
+        notebook.emit("size-allocate", Gdk.Rectangle())
+        self.assertEqual(picture_scroller.restore_position, -1)
         self.assertFalse(picture_scroller.last_result_succeed)
-        self.assertTrue(picture_scroller.first_run)
         # success (but last result was a fail - idle_add pic_pane set_position)
         restore_pos = picture_scroller.restore_position
+        self.assertFalse(picture_scroller.restore_pic_pane)
         picture_scroller._hide_restore_pic_pane([1])
-        picture_scroller.pic_pane.emit("size-allocate", Gdk.Rectangle())
-        update_gui()
+        self.assertTrue(picture_scroller.restore_pic_pane)
+        notebook.emit("size-allocate", Gdk.Rectangle())
+        self.assertFalse(picture_scroller.restore_pic_pane)
         self.assertTrue(picture_scroller.last_result_succeed)
-        self.assertTrue(picture_scroller.first_run)
         # no result (yet records restore position because prev was successful)
         picture_scroller._hide_restore_pic_pane([])
-        picture_scroller.pic_pane.emit("size-allocate", Gdk.Rectangle())
-        update_gui()
+        notebook.emit("size-allocate", Gdk.Rectangle())
         self.assertFalse(picture_scroller.last_result_succeed)
         restore_pos2 = picture_scroller.restore_position
         self.assertIsNotNone(restore_pos2)
         self.assertNotEqual(restore_pos2, restore_pos)
-        self.assertTrue(picture_scroller.first_run)
         # succeed and change position then succeed again should store new value
         picture_scroller._hide_restore_pic_pane([1])
-        picture_scroller.pic_pane.emit("size-allocate", Gdk.Rectangle())
-        update_gui()
+        notebook.emit("size-allocate", Gdk.Rectangle())
         picture_scroller.pic_pane.set_position(100)
         self.assertTrue(picture_scroller.last_result_succeed)
         picture_scroller._hide_restore_pic_pane([1])
-        picture_scroller.pic_pane.emit("size-allocate", Gdk.Rectangle())
-        update_gui()
+        notebook.emit("size-allocate", Gdk.Rectangle())
         self.assertEqual(picture_scroller.restore_position, 100)
         self.assertTrue(picture_scroller.last_result_succeed)
-        self.assertFalse(picture_scroller.first_run)
         # change position then fail multiple time, captures and uses last
         # successful position
         picture_scroller.pic_pane.set_position(50)
         picture_scroller._hide_restore_pic_pane(None)
-        picture_scroller.pic_pane.emit("size-allocate", Gdk.Rectangle())
-        update_gui()
+        notebook.emit("size-allocate", Gdk.Rectangle())
         self.assertFalse(picture_scroller.last_result_succeed)
         picture_scroller._hide_restore_pic_pane(None)
-        picture_scroller.pic_pane.emit("size-allocate", Gdk.Rectangle())
-        update_gui()
+        notebook.emit("size-allocate", Gdk.Rectangle())
         self.assertFalse(picture_scroller.last_result_succeed)
         picture_scroller._hide_restore_pic_pane([])
-        picture_scroller.pic_pane.emit("size-allocate", Gdk.Rectangle())
-        update_gui()
+        notebook.emit("size-allocate", Gdk.Rectangle())
         self.assertFalse(picture_scroller.last_result_succeed)
         self.assertEqual(picture_scroller.restore_position, 50)
         picture_scroller._hide_restore_pic_pane([1])
-        picture_scroller.pic_pane.emit("size-allocate", Gdk.Rectangle())
-        update_gui()
-        # Wait for timeout
-        time.sleep(0.5)
-        update_gui()
+        notebook.emit("size-allocate", Gdk.Rectangle())
         self.assertTrue(picture_scroller.last_result_succeed)
         self.assertEqual(picture_scroller.restore_position, 50)
         self.assertEqual(picture_scroller.pic_pane.get_position(), 50)