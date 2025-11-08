72a992a7e951c0d7f19ddd376ace5fbdb65d904d
RoDuth <rossdemuth123@gmail.com>
2025-11-08 22:32:22 +1000

    NoteBox spell checking, temp fix mingw
    Get `enchant_set_prefix_dir()` to work as it does on other platforms.
    
    see:
    b9148c89
    7340f927
    https://github.com/msys2/MINGW-packages/issues/22860
    https://github.com/rrthomas/enchant/issues/405
    https://github.com/msys2/MINGW-packages/pull/22865

--- scripts/extra-hooks/hook-gi.repository.Gspell.py
+++ scripts/extra-hooks/hook-gi.repository.Gspell.py
@@ -4,58 +4,29 @@ import os
 from PyInstaller.utils.hooks.gi import GiModuleInfo
 
 module_info = GiModuleInfo("Gspell", "1")
-if module_info.available:
-    binaries, datas, hiddenimports = module_info.collect_typelib_data()
+binaries, datas, hiddenimports = module_info.collect_typelib_data()
 
-    msystem_prefix = os.environ.get("MSYSTEM_PREFIX")
-    if msystem_prefix:
-        # Collect enchant providers (e.g. libenchant_aspell.dll) Enchant on
-        # windows will look for providers in ../lib/enchant-2 relative to the
-        # enchant library. Pyinstaller puts libs in the root of the bundle. So
-        # we will need to move them manually later for this to work.
-        # enchant_set_prefix_dir() is not working in the current mingw version.
-        enchant_plugin_dir = os.path.join(msystem_prefix, "lib", "enchant-2")
-        for f in glob.glob(os.path.join(enchant_plugin_dir, "*.dll")):
-            binaries.append((f, "lib/enchant-2"))
+root = os.path.dirname(module_info.get_libdir())
 
-        # collect the enchant.ordering file and place it within the plugin dir
-        # Requires a runtime hook to set ENCHANT_CONFIG_DIR environment
-        # variable
-        enchant_ordering = os.path.join(
-            msystem_prefix,
-            "share",
-            "enchant-2",
-            "enchant.ordering",
-        )
-        if os.path.isfile(enchant_ordering):
-            datas.append((enchant_ordering, "share/enchant-2"))
+# Collect enchant providers (e.g. libenchant_aspell.dll)
+enchant_plugin_dir = os.path.join(
+    root,
+    "lib",
+    "enchant-2/",
+)
+for f in glob.glob(os.path.join(enchant_plugin_dir, "*.*")):
+    binaries.append((f, "lib/enchant-2"))
 
-        # Collect aspell dictionaries
-        aspell_data_path = os.path.join(msystem_prefix, "lib", "aspell-0.60")
-        for f in glob.glob(os.path.join(aspell_data_path, "*.*")):
-            datas.append((f, "lib/aspell-0.60"))
-    else:
-        # Collect enchant providers
-        enchant_plugin_dir = os.path.join(
-            module_info.get_libdir(),
-            "enchant-2/",
-        )
-        for f in glob.glob(os.path.join(enchant_plugin_dir, "*.*")):
-            binaries.append((f, "lib/enchant-2"))
+# Collect config files
+enchant_configs = os.path.join(
+    root,
+    "share",
+    "enchant-2",
+)
+for f in glob.glob(os.path.join(enchant_configs, "*.*")):
+    datas.append((f, "share/enchant-2"))
 
-        # Collect config files
-        enchant_configs = os.path.join(
-            module_info.get_libdir(),
-            "..",
-            "share",
-            "enchant-2",
-        )
-        for f in glob.glob(os.path.join(enchant_configs, "*.*")):
-            datas.append((f, "share/enchant-2"))
-
-        # Collect aspell dictionaries
-        aspell_data_path = os.path.join(
-            module_info.get_libdir(), "aspell-0.60"
-        )
-        for f in glob.glob(os.path.join(aspell_data_path, "*.*")):
-            datas.append((f, "lib/aspell-0.60"))
+# Collect aspell dictionaries
+aspell_data_path = os.path.join(root, "lib", "aspell-0.60")
+for f in glob.glob(os.path.join(aspell_data_path, "*.*")):
+    datas.append((f, "lib/aspell-0.60"))
--- scripts/extra-hooks/pyi_rth_gspell.py
+++ scripts/extra-hooks/pyi_rth_gspell.py
@@ -6,15 +6,13 @@ import sys
 # NOTE we have to move lib/enchant-2 to beside _internal for the providers to
 # be discovered (libenchant looks relative to its current position i.e.
 # ../lib/enchant-2/)
+lib_name = "libenchant-2.so.2"
 if sys.platform == "win32":
-    config_dir = os.path.join(
-        os.path.dirname(sys._MEIPASS),
-        "share",
-        "enchant-2",
-    )
-    os.environ["ENCHANT_CONFIG_DIR"] = config_dir
-else:
-    # relocate
-    enchant_lib_path = os.path.join(sys._MEIPASS, "libenchant-2.2.dylib")
-    enchant_lib = ctypes.cdll.LoadLibrary(enchant_lib_path)
-    enchant_lib.enchant_set_prefix_dir(sys._MEIPASS.encode())
+    lib_name = "libenchant-2-2.dll"
+elif sys.platform == "darwin":
+    lib_name = "libenchant-2.2.dylib"
+
+# relocate enchant
+enchant_lib_path = os.path.join(sys._MEIPASS, lib_name)
+enchant_lib = ctypes.cdll.LoadLibrary(enchant_lib_path)
+enchant_lib.enchant_set_prefix_dir(sys._MEIPASS.encode())
